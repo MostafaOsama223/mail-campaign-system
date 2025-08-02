08e67b4ad79af674c0483ef5a8da4b047e3952c7
Mostafa Osama <m.osama@tamhub.com>
2025-08-02 21:21:37 +0300

    feat: add user-service microservice

---
+++ user-service/src/main/java/com/mail_campaign_system/user_service/UserServiceApplication.java
@@ -0,0 +1,11 @@
+package com.mail_campaign_system.user_service;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
