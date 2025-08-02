8dc28b192d7b37ea3f9afe2e3d93a23e2b4f9dc4
Mostafa Osama <m.osama@tamhub.com>
2025-08-03 02:56:07 +0300

    change: TripCreatedEventFanOutFunction to use user service

--- mail-fanount-service/src/main/java/com/mail_campaign_system/mail_fanout_service/function/TripCreatedEventFanOutFunction.java
+++ mail-fanount-service/src/main/java/com/mail_campaign_system/mail_fanout_service/function/TripCreatedEventFanOutFunction.java
@@ -1,13 +1,18 @@
 package com.mail_campaign_system.mail_fanout_service.function;
 
 import com.mail_campaign_system.mail_fanout_service.dto.FanOutTask;
+import com.mail_campaign_system.mail_fanout_service.dto.GetUserContactStatistics;
 import com.mail_campaign_system.mail_fanout_service.dto.TripCreatedEvent;
+import com.mail_campaign_system.mail_fanout_service.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,23 +27,37 @@ public class TripCreatedEventFanOutFunction {
     private int batchSize;
 
     @Bean
-    public Function<TripCreatedEvent, List<Message<FanOutTask>>> tripCreatedEventConsumer() {
+    public UserService userService() {
+        RestClient restClient = RestClient.builder()
+                .baseUrl("http://localhost:8095/api/v1")
+                .build();
+
+        RestClientAdapter adapter = RestClientAdapter.create(restClient);
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(adapter).build();
+
+        return factory.createClient(UserService.class);
+    }
+
+    @Bean
+    public Function<TripCreatedEvent, List<Message<FanOutTask>>> tripCreatedEventConsumer(UserService userService) {
         return tripCreatedEvent -> {
 
-//            TODO: Integrate with UserService to fetch user contacts
-            long totalNumberOfUsers = 6000;
-            long numberOfBatches = (totalNumberOfUsers + batchSize - 1) / batchSize;
+            GetUserContactStatistics userContactStatistics = userService.getUserContactStatistics();
+
+            int totalNumberOfUsers = userContactStatistics.totalContactsCount();
+            int firstContactId = userContactStatistics.firstContactId();
+            int numberOfBatches = (totalNumberOfUsers + batchSize - 1) / batchSize;
 
             log.info("TripCreatedEvent received {}. Batch size: {}", tripCreatedEvent, batchSize);
 
-            List<Message<FanOutTask>> fanOutTasks = new ArrayList<>((int) numberOfBatches);
+            List<Message<FanOutTask>> fanOutTasks = new ArrayList<>(numberOfBatches);
 
             for (int i = 0; i < numberOfBatches; i++) {
                 FanOutTask fanOutTask = new FanOutTask(
                         UUID.randomUUID(),
                         tripCreatedEvent.tripId(),
-                        i * batchSize,
-                        Math.min((i + 1) * batchSize - 1, (int) totalNumberOfUsers - 1)
+                        i * batchSize + firstContactId,
+                        Math.min((i + 1) * batchSize + firstContactId - 1, totalNumberOfUsers - 1)
                 );
                 fanOutTasks.add(MessageBuilder.withPayload(fanOutTask).build());
 
